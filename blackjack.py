285acbe5ab28a080ecc555c58291e3f952d64efe
davidbison <davidbisonhubert@gmail.com>
2016-03-04 14:26:15 -0800

    Reposition items drawn on canvas

--- blackjack.py
+++ blackjack.py
@@ -198,8 +198,12 @@ def draw(canvas):
     canvas.draw_text("Blackjack", [600 // 19, 75], 56, "#000", 'sans-serif')
     canvas.draw_text(game_message, [600 // 2, 600 // 1.5 - 10], 36, "#000", 'sans-serif')
     canvas.draw_text("Score: " + str(score), [364, 75], 36, "#000", 'sans-serif')
-    canvas.draw_text("Player", [600 // 5, 600 // 1.5 - 10], 36, "#000", 'sans-serif')
+    canvas.draw_text("Player", [600 // 7, 600 // 1.5 - 10], 36, "#000", 'sans-serif')
     canvas.draw_text("Dealer", [600 // 7, 600 // 3.5 - 10], 36, "#000", 'sans-serif')
+
+    # draw player and dealer hands
+    player_hand.draw(canvas, [600 // 7, 600 // 1.5])
+    dealer_hand.draw(canvas, [600 // 7, 600 // 3.5])
 
     # draw player and dealer hands
     player_hand.draw(canvas, [600 // 5, 600 // 1.5])
